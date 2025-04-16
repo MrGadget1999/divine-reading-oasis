ec15c61c4e4afd620ee16f52722babc715cce6e9
MrGadget1999 <ivanikavungal@gmail.com>
2025-04-17 05:22:44 +0530

    Fix routing for GitHub Pages

---
+++ src/pages/Test.tsx
@@ -0,0 +1,8 @@
+export default function Test() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl">Test Page</h1>
+      <p>If you can see this, routing is working!</p>
+    </div>
+  );
+}
